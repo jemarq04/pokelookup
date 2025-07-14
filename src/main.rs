0e8b54c49c8795452fc1da8e32a2ee1dac2ab472
Justin Marquez <justin.marquez@wisc.edu>
2025-07-13 22:17:43 -0500

    feat: add ability lookup

--- src/main.rs
+++ src/main.rs
@@ -35,10 +35,9 @@ async fn main() {
     println!("{:?}", args);
 
     match args.command {
-        SubArgs::TypeCmd{..} => {
-            print_types(&args.command).await;
-        },
-        _ => panic!("error: not yet implemented"),
+        SubArgs::TypeCmd{..} => print_types(&args.command).await,
+        SubArgs::AbilityCmd{..} => print_abilities(&args.command).await,
+        //_ => panic!("error: not yet implemented"),
     };
 }
 
@@ -64,7 +63,7 @@ async fn print_types(args: &SubArgs) {
         Err(_) => panic!("error: could not retrive types for pokemon {}", pokemon),
     };
 
-    // Return English names for types
+    // Print English names
     let result = match future::try_join_all(types.into_iter().map(|t| t.names).map(
             async |names| {
                 for n in names.iter() {
@@ -79,5 +78,50 @@ async fn print_types(args: &SubArgs) {
         Err(_) => panic!("error: could not find English names for types"),
     };
 
+    println!("{}: {:?}", pokemon, result);
+}
+
+async fn print_abilities(args: &SubArgs) {
+    let SubArgs::AbilityCmd{pokemon} = args else {
+        panic!("error: incorrect inputs");
+    };
+
+    // Create client
+    let client = rustemon::client::RustemonClient::default();
+
+    // Create pokemon resource
+    let mon_resource = match pokemon::get_by_name(&pokemon, &client).await {
+        Ok(x) => x,
+        Err(_) => panic!("error: could not find pokemon {}", pokemon),
+    };
+
+    // Create struct to store ability
+    struct Ability {
+        hidden: bool,
+        ability: rustemon::model::pokemon::Ability,
+    }
+
+    // Get abilities
+    let abilities = match future::try_join_all(
+        mon_resource.abilities.iter().map(async |a| {
+            match a.ability.follow(&client).await {
+                Ok(x) => Ok(Ability{hidden: a.is_hidden, ability: x}),
+                Err(_) => Err(()),
+            }
+        })
+    ).await {
+        Ok(x) => x,
+        Err(_) => panic!("error: could not retrive abilities for pokemon {}", pokemon),
+    };
+
+    // Print English names
+    let mut result = Vec::new();
+    for ab in abilities.into_iter() {
+        for n in ab.ability.names.iter() {
+            if let Ok(x) = n.language.follow(&client).await && x.name == "en" {
+                result.push(n.name.clone() + if ab.hidden {" (Hidden)"} else {""});
+            }
+        }
+    }
     println!("{}: {:?}", pokemon, result);
 }
