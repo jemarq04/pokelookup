696a1ecdc5e077ff328c11cfb392ebc036c92061
Justin Marquez <justin.marquez@wisc.edu>
2025-08-14 18:37:50 -0500

    feat: add pokearth functionality to dex subcommand

--- src/lookup/dex.rs
+++ src/lookup/dex.rs
@@ -74,11 +74,53 @@ pub fn open_pokedex(pokemon: String, generation: Option<i64>) -> Result<Vec<Stri
 }
 
 pub fn open_pokearth(
-  location: String,
+  region: String,
   area: Option<String>,
   generation: Option<i64>,
 ) -> Result<Vec<String>, clap::Error> {
-  Ok(Vec::new())
+  let region = region.to_lowercase();
+  let area = match area {
+    Some(x) => Some(x.to_lowercase().replace(" ", "")),
+    None => None,
+  };
+
+  let mut url = format!("https://www.serebii.net/pokearth/{region}/");
+  if let Some(area) = &area {
+    url += &format!(
+      "{}{}.shtml",
+      match generation {
+        None | Some(0) => String::new(),
+        Some(g) => match g {
+          1 => String::from("1st/"),
+          2 => String::from("2nd/"),
+          3 => String::from("3rd/"),
+          g @ 4..=LATEST_GEN => format!("{g}th/"),
+          _ => {
+            return Err(cli::error(
+              ErrorKind::InvalidValue,
+              format!("invalid generation: {g}"),
+            ));
+          },
+        },
+      },
+      area,
+    );
+  }
+
+  if let Err(_) = open::that(url) {
+    return Err(cli::error(
+      ErrorKind::InvalidValue,
+      format!(
+        "couldn't open page for {}",
+        match area {
+          Some(area) => area,
+          None => region,
+        },
+      ),
+    ));
+  }
+
+  Ok(svec!["Opened page successfully."])
 }
 
 pub fn open_attackdex() -> Result<Vec<String>, clap::Error> {
