6a5c7a6ea7d7b5fbfd20dbc72405d1622738e0a5
Justin Marquez <justin.marquez@wisc.edu>
2026-02-05 13:32:18 -0600

    test: add initial CLI testing

--- src/main.rs
+++ src/main.rs
@@ -141,4 +141,13 @@ async fn main() {
 #[cfg(test)]
 mod tests {
   use super::*;
+
+  #[test]
+  fn test_cli_varieties() {
+    let args = svec![get_appname(), "list", "-f", "meowth"];
+    match Args::try_parse_from(args) {
+      Err(e) => panic!("{}", e.render()),
+      Ok(_) => {},
+    }
+  }
 }
