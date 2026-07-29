02ee0dede1ef8cd1a39d8a9966edaeb4b5efb582
Justin Marquez <justin.marquez@wisc.edu>
2026-07-29 16:29:25 -0300

    fix: add missing argument structs

--- src/utils/cli.rs
+++ src/utils/cli.rs
@@ -93,6 +93,229 @@ pub enum Command {
   WebCmd(WebArgs),
 }
 
+// Arguments for each subcommand
+#[derive(Args, Debug)]
+pub struct ListArgs {
+  #[arg(help = "name of pokemon species")]
+  pub pokemon: String,
+
+  #[arg(short, long, help = "skip API requests for formatted names")]
+  pub fast: bool,
+
+  #[arg(value_enum,
+    short = 'L',
+    long,
+    value_name = "LANGUAGE",
+    default_value_t = LanguageId::En,
+    hide_possible_values=true,
+    help = "language ID for API requests for formatted names"
+  )]
+  pub lang: LanguageId,
+}
+
+#[derive(Args, Debug)]
+pub struct TypeArgs {
+  #[arg(help = "name of pokemon")]
+  pub pokemon: String,
+
+  #[arg(short, long, help = "skip API requests for formatted names")]
+  pub fast: bool,
+
+  #[arg(short, long = "gen", help = "generation to query")]
+  pub generation: Option<i64>,
+
+  #[arg(value_enum,
+    short = 'L',
+    long,
+    value_name = "LANGUAGE",
+    default_value_t = LanguageId::En,
+    hide_possible_values=true,
+    help = "language ID for API requests for formatted names"
+  )]
+  pub lang: LanguageId,
+
+  #[arg(short, help = "recursively check evolution chain")]
+  pub recursive: bool,
+}
+
+#[derive(Args, Debug)]
+pub struct AbilityArgs {
+  #[arg(help = "name of pokemon")]
+  pub pokemon: String,
+
+  #[arg(short, long, help = "skip API requests for formatted names")]
+  pub fast: bool,
+
+  #[arg(value_enum,
+    short = 'L',
+    long,
+    value_name = "LANGUAGE",
+    default_value_t = LanguageId::En,
+    hide_possible_values=true,
+    help = "language ID for API requests for formatted names"
+  )]
+  pub lang: LanguageId,
+
+  #[arg(short, help = "recursively check evolution chain")]
+  pub recursive: bool,
+}
+
+#[derive(Args, Debug)]
+pub struct MoveArgs {
+  #[arg(help = "name of pokemon")]
+  pub pokemon: String,
+
+  #[arg(short, long, help = "skip API requests for formatted names")]
+  pub fast: bool,
+
+  #[arg(value_enum,
+    short = 'L',
+    long,
+    value_name = "LANGUAGE",
+    default_value_t = LanguageId::En,
+    hide_possible_values=true,
+    help = "language ID for API requests for formatted names"
+  )]
+  pub lang: LanguageId,
+
+  #[arg(value_enum, short, long, default_value_t=VersionGroup::ScarletViolet,
+          hide_possible_values=true, help="version group name")]
+  pub vgroup: VersionGroup,
+
+  #[arg(short, long, help = "request default moveset at given level")]
+  pub level: Option<i64>,
+}
+
+#[derive(Args, Debug)]
+pub struct EggArgs {
+  #[arg(help = "name of pokemon species")]
+  pub pokemon: String,
+
+  #[arg(short, long, help = "skip API requests for formatted names")]
+  pub fast: bool,
+
+  #[arg(value_enum,
+    short = 'L',
+    long,
+    value_name = "LANGUAGE",
+    default_value_t = LanguageId::En,
+    hide_possible_values=true,
+    help = "language ID for API requests for formatted names"
+  )]
+  pub lang: LanguageId,
+}
+
+#[derive(Args, Debug)]
+pub struct GenderArgs {
+  #[arg(help = "name of pokemon species")]
+  pub pokemon: String,
+
+  #[arg(short, long, help = "skip API requests for formatted names")]
+  pub fast: bool,
+
+  #[arg(value_enum,
+    short = 'L',
+    long,
+    value_name = "LANGUAGE",
+    default_value_t = LanguageId::En,
+    hide_possible_values=true,
+    help = "language ID for API requests for formatted names"
+  )]
+  pub lang: LanguageId,
+}
+
+#[derive(Args, Debug)]
+pub struct EncounterArgs {
+  #[arg(value_enum, hide_possible_values = true, help = "name of version")]
+  pub version: Version,
+
+  #[arg(help = "name of pokemon")]
+  pub pokemon: String,
+
+  #[arg(short, long, help = "skip API requests for formatted names")]
+  pub fast: bool,
+
+  #[arg(value_enum,
+    short = 'L',
+    long,
+    value_name = "LANGUAGE",
+    default_value_t = LanguageId::En,
+    hide_possible_values=true,
+    help = "language ID for API requests for formatted names"
+  )]
+  pub lang: LanguageId,
+
+  #[arg(short, help = "recursively check evolution chain")]
+  pub recursive: bool,
+
+  #[arg(short, long, help = "condense output by only showing location areas")]
+  pub condensed: bool,
+}
+
+#[derive(Args, Debug)]
+pub struct EvolutionArgs {
+  #[arg(help = "name of pokemon species")]
+  pub pokemon: String,
+
+  #[arg(short, long, help = "skip API requests for formatted names")]
+  pub fast: bool,
+
+  #[arg(value_enum,
+    short = 'L',
+    long,
+    value_name = "LANGUAGE",
+    default_value_t = LanguageId::En,
+    hide_possible_values=true,
+    help = "language ID for API requests for formatted names"
+  )]
+  pub lang: LanguageId,
+
+  #[arg(
+    short,
+    long,
+    help = "hide the names of the pokemon in the evolution chain"
+  )]
+  pub secret: bool,
+
+  #[arg(short, long, help = "show all evolution chains, even outdated ones")]
+  pub all: bool,
+}
+
+#[derive(Args, Debug)]
+pub struct MatchupArgs {
+  #[arg(
+    value_enum,
+    hide_possible_values = true,
+    value_name = "TYPE",
+    help = "name of type"
+  )]
+  pub primary: Type,
+
+  #[arg(
+    value_enum,
+    hide_possible_values = true,
+    value_name = "TYPE",
+    help = "name of optional secondary type"
+  )]
+  pub secondary: Option<Type>,
+
+  #[arg(short, long, help = "print output as a list instead of a table")]
+  pub list: bool,
+
+  #[arg(short, long, help = "skip API requests for formatted names")]
+  pub fast: bool,
+
+  #[arg(value_enum,
+    short = 'L',
+    long,
+    value_name = "LANGUAGE",
+    default_value_t = LanguageId::En,
+    hide_possible_values=true,
+    help = "language ID for API requests for formatted names"
+  )]
+  pub lang: LanguageId,
+}
+
 #[derive(Args, Debug)]
 pub struct WebArgs {
   #[command(flatten)]
