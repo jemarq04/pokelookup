613e0c3ae98647c9b5d4f0c98483719fa703f28b
Justin Marquez <37006684+jemarq04@users.noreply.github.com>
2026-09-20 16:01:24 -0500

    fix: Update rustemon version to match API changes and fix three-stage evolution chains (#32)
    * refactor: Lookup method refactoring (#16)
    
    * refactor: rename structs to match conventions
    
    * refactor: use ListArgs for cleaner code
    
    * refactor: use TypeArgs for cleaner code
    
    * refactor: use AbilityArgs for cleaner code
    
    * refactor: clean up moves
    
    * refactor: clean up eggs
    
    * refactor: clean up genders
    
    * refactor: clean up encounters
    
    * refactor: clean up evolutions
    
    * refactor: clean up type matchups
    
    * refactor: clean up web args
    
    * fix: add missing argument structs
    
    * feat: bump version number
    
    * docs: add comments to separate chunks of file
    
    * refactor: split argument structs into their own module for easier readability
    
    * fix: set WebArgs to 'web' feature of crate
    
    * feat: Add DLC flag for encounters and sort output (#17)
    
    * feat: add --include-dlcs option to look up encounters in base game and its DLCs
    
    * feat: change behavior to only work when specifying the base version
    
    * feat: sort encounters by location area
    
    * test: sort encounters by location area
    
    * feat: bump version to 1.5.0
    
    * lint: Apply pedantic lints (#18)
    
    * lint: fix pedantic lints
    
    * refactor: break apart matchups function into smaller functions
    
    * chore: update itertools version
    
    * ci: add pedantic linting makefile target
    
    * fix: Correct base/evolved form treatment for 'secret' evolutions (#20)
    
    * fix: make the base/evolved form secret, if requested
    
    * lint: avoid making a reference only to immediately dereference it in function call
    
    * feat: bump version number
    
    * test: update evolutions test to include fast check to consider base/evolved forms
    
    * lint: update too-many-lines-threshold
    
    * chore(deps): bump actions/checkout from 4 to 7 (#23)
    
    Bumps [actions/checkout](https://github.com/actions/checkout) from 4 to 7.
    - [Release notes](https://github.com/actions/checkout/releases)
    - [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v4...v7)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-version: '7'
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix: correct ability ordering based on slot
    
    * chore(deps): bump rust dependencies
    
    * chore: bump version number
    
    * refactor: rename custom ability struct
    
    * fix: Update rustemon wrapper to handle new schema changes (#31)
    
    * feat: use renamed EvolutionDetail struct fields to access PokemonForm objects and get their names
    
    * feat: add new evolution detail fields (allowed_natures and condition_expression)
    
    * test: add tests for new changes to evolution details
    
    * feat: update rustemon version
    
    fixes errors with testing due to changes with API schema!
    
    * fix: Correct evolutions subcommand for three-stage chains with form requirements (#33)
    
    * test: add test for intended feature
    
    * fix: correct three-stage regional evolution chains
    
    * test: remove test for unplanned feature
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/lookup/evolutions.rs
+++ src/lookup/evolutions.rs
@@ -80,13 +80,20 @@ pub async fn print_evolutions(
           result.push(format!(
             "{} -> {}",
             if !args.secret
-              && let Some(base_form_resource) = &details1.base_form
+              && let Some(required_form_resource) = &details1.required_pokemon_form
             {
               if args.fast {
-                base_form_resource.name.clone()
+                required_form_resource.name.clone()
               } else {
-                let base_form = base_form_resource.follow(client).await.unwrap();
-                helpers::get_pokemon_name(client, &base_form, &args.lang.to_string()).await
+                let required_pokemon = required_form_resource
+                  .follow(client)
+                  .await
+                  .unwrap()
+                  .pokemon
+                  .follow(client)
+                  .await
+                  .unwrap();
+                helpers::get_pokemon_name(client, &required_pokemon, &args.lang.to_string()).await
               }
             } else {
               helpers::get_evolution_name(
@@ -116,17 +123,28 @@ pub async fn print_evolutions(
             let _ = write!(result.last_mut().unwrap(), " ({details_str})");
           }
 
+          let evolved_form_name = match details1.evolved_pokemon_form.clone() {
+            Some(val) => val.name,
+            None => String::new(),
+          };
           let _ = write!(
             result.last_mut().unwrap(),
             " -> {}",
             if !args.secret
-              && let Some(evolved_form_resource) = &details1.evolved_form
+              && let Some(evolved_form_resource) = &details1.evolved_pokemon_form
             {
               if args.fast {
                 evolved_form_resource.name.clone()
               } else {
-                let evolved_form = evolved_form_resource.follow(client).await.unwrap();
-                helpers::get_pokemon_name(client, &evolved_form, &args.lang.to_string()).await
+                let evolved_pokemon = evolved_form_resource
+                  .follow(client)
+                  .await
+                  .unwrap()
+                  .pokemon
+                  .follow(client)
+                  .await
+                  .unwrap();
+                helpers::get_pokemon_name(client, &evolved_pokemon, &args.lang.to_string()).await
               }
             } else {
               helpers::get_evolution_name(
@@ -148,6 +166,14 @@ pub async fn print_evolutions(
               if !args.all && !details2.is_default {
                 continue;
               }
+              let required_form_name = match details2.required_pokemon_form.clone() {
+                Some(val) => val.name,
+                None => String::new(),
+              };
+              if evolved_form_name != required_form_name {
+                continue;
+              }
+
               let mut temp_steps: String = format!(
                 " -> {}",
                 if args.fast {
@@ -168,13 +194,21 @@ pub async fn print_evolutions(
                 temp_steps,
                 " -> {}",
                 if !args.secret
-                  && let Some(evolved_form_resource) = &details2.evolved_form
+                  && let Some(evolved_form_resource) = &details2.evolved_pokemon_form
                 {
                   if args.fast {
                     evolved_form_resource.name.clone()
                   } else {
-                    let evolved_form = evolved_form_resource.follow(client).await.unwrap();
-                    helpers::get_pokemon_name(client, &evolved_form, &args.lang.to_string()).await
+                    let evolved_pokemon = evolved_form_resource
+                      .follow(client)
+                      .await
+                      .unwrap()
+                      .pokemon
+                      .follow(client)
+                      .await
+                      .unwrap();
+                    helpers::get_pokemon_name(client, &evolved_pokemon, &args.lang.to_string())
+                      .await
                   }
                 } else {
                   helpers::get_evolution_name(
@@ -422,6 +456,91 @@ mod tests {
         Ok(res) => assert_eq!(res, vals),
         Err(err) => panic!("{}", err.render()),
       }
+    }
+  }
+
+  #[tokio::test]
+  async fn test_evolutions_regional_forms_more() {
+    let client = RustemonClient::default();
+
+    let success = vec![
+      vec![
+        "mime-jr -> level-up (known_move: mimic) -> mr-mime",
+        "mime-jr -> level-up (known_move: mimic, region: galar) -> mr-mime-galar -> level-up (min_level: 42) -> mr-rime",
+      ],
+      vec![
+        "Mime Jr. -> Level up (known_move: Mimic) -> Mr. Mime",
+        "Mime Jr. -> Level up (known_move: Mimic, region: Galar) -> Galarian Mr. Mime -> Level up (min_level: 42) -> Mr. Rime",
+      ],
+    ];
+
+    for (idx, vals) in success.into_iter().enumerate() {
+      let args = EvolutionArgs {
+        pokemon: String::from("mr-mime"),
+        fast: idx == 0,
+        lang: LanguageId::En,
+        secret: false,
+        all: false,
+      };
+
+      match print_evolutions(&client, args).await {
+        Ok(res) => assert_eq!(res, vals),
+        Err(err) => panic!("{}", err.render()),
+      }
+    }
+  }
+
+  #[tokio::test]
+  async fn test_evolutions_allowed_natures() {
+    let client = RustemonClient::default();
+
+    let success = vec![
+      vec![
+        "toxel -> level-up (min_level: 30, allowed_natures: hardy/docile/hasty/adamant/impish/rash/jolly/naughty/lax/quirky/naive/brave/sassy) -> toxtricity-amped",
+        "toxel -> level-up (min_level: 30, allowed_natures: bold/modest/calm/timid/lonely/mild/gentle/bashful/careful/relaxed/quiet/serious) -> toxtricity-low-key",
+      ],
+      vec![
+        "Toxel -> Level up (min_level: 30, allowed_natures: Hardy/Docile/Hasty/Adamant/Impish/Rash/Jolly/Naughty/Lax/Quirky/Naive/Brave/Sassy) -> Amped Toxtricity",
+        "Toxel -> Level up (min_level: 30, allowed_natures: Bold/Modest/Calm/Timid/Lonely/Mild/Gentle/Bashful/Careful/Relaxed/Quiet/Serious) -> Low Key Toxtricity",
+      ],
+    ];
+
+    for (idx, vals) in success.into_iter().enumerate() {
+      let args = EvolutionArgs {
+        pokemon: String::from("toxel"),
+        fast: idx == 0,
+        lang: LanguageId::En,
+        secret: false,
+        all: false,
+      };
+
+      match print_evolutions(&client, args).await {
+        Ok(res) => assert_eq!(res, vals),
+        Err(err) => panic!("{}", err.render()),
+      }
+    }
+  }
+
+  #[tokio::test]
+  async fn test_evolution_condition_expressions() {
+    let client = RustemonClient::default();
+
+    let success = vec![
+      "dunsparce -> level-up (known_move: hyper-drill, percentage_chance: 99%) -> dudunsparce-two-segment",
+      "dunsparce -> level-up (known_move: hyper-drill, percentage_chance: 1%) -> dudunsparce-three-segment",
+    ];
+
+    let args = EvolutionArgs {
+      pokemon: String::from("dunsparce"),
+      fast: true,
+      lang: LanguageId::En,
+      secret: false,
+      all: false,
+    };
+
+    match print_evolutions(&client, args).await {
+      Ok(res) => assert_eq!(res, success),
+      Err(err) => panic!("{}", err.render()),
     }
   }
 }
