2e932bd524d4435dcdb5c3317bd99c6368cc83b4
Justin Marquez <jmarquez1014@gmail.com>
2026-09-20 16:13:22 -0400

    test: add test for intended feature

--- src/lookup/evolutions.rs
+++ src/lookup/evolutions.rs
@@ -448,6 +448,68 @@ mod tests {
   }
 
   #[tokio::test]
+  async fn test_evolutions_regional_forms_more() {
+    let client = RustemonClient::default();
+
+    let success = vec![
+      vec![
+        "mime-jr -> level-up (known_move: mimic) -> mr-mime",
+        "mime-jr -> level-up (known_move: mimic, region: galar) -> mr-mime-galar -> level-up (min_level: 42) -> mr-rime",
+      ],
+      vec![
+        "Mime Jr. -> Level up (known_move: Mimic) -> Mr. Mime",
+        "Mime Jr. -> Level up (known_move: Mimic, region: Galar) -> Galarian Mr. Mime -> Level up (min_level: 42) -> Mr. Rime",
+      ],
+    ];
+
+    for (idx, vals) in success.into_iter().enumerate() {
+      let args = EvolutionArgs {
+        pokemon: String::from("mr-mime"),
+        fast: idx == 0,
+        lang: LanguageId::En,
+        secret: false,
+        all: false,
+      };
+
+      match print_evolutions(&client, args).await {
+        Ok(res) => assert_eq!(res, vals),
+        Err(err) => panic!("{}", err.render()),
+      }
+    }
+  }
+
+  #[tokio::test]
+  async fn test_evolutions_regional_forms_with_and_without_chain() {
+    let client = RustemonClient::default();
+
+    let success = vec![
+      vec![
+        "corsola",
+        "corsola-galar -> level-up (min_level: 38) -> cursola",
+      ],
+      vec![
+        "Corsola",
+        "Galarian Corsola -> Level up (min_level: 38) -> Cursola",
+      ],
+    ];
+
+    for (idx, vals) in success.into_iter().enumerate() {
+      let args = EvolutionArgs {
+        pokemon: String::from("corsola"),
+        fast: idx == 0,
+        lang: LanguageId::En,
+        secret: false,
+        all: false,
+      };
+
+      match print_evolutions(&client, args).await {
+        Ok(res) => assert_eq!(res, vals),
+        Err(err) => panic!("{}", err.render()),
+      }
+    }
+  }
+
+  #[tokio::test]
   async fn test_evolutions_allowed_natures() {
     let client = RustemonClient::default();
 
