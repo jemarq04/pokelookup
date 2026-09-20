d1a4829dcd3883f72dc2a8327f7956e53aa577d5
Justin Marquez <jmarquez1014@gmail.com>
2026-09-20 16:52:22 -0400

    test: remove test for unplanned feature

--- src/lookup/evolutions.rs
+++ src/lookup/evolutions.rs
@@ -491,37 +491,6 @@ mod tests {
   }
 
   #[tokio::test]
-  async fn test_evolutions_regional_forms_with_and_without_chain() {
-    let client = RustemonClient::default();
-
-    let success = vec![
-      vec![
-        "corsola",
-        "corsola-galar -> level-up (min_level: 38) -> cursola",
-      ],
-      vec![
-        "Corsola",
-        "Galarian Corsola -> Level up (min_level: 38) -> Cursola",
-      ],
-    ];
-
-    for (idx, vals) in success.into_iter().enumerate() {
-      let args = EvolutionArgs {
-        pokemon: String::from("corsola"),
-        fast: idx == 0,
-        lang: LanguageId::En,
-        secret: false,
-        all: false,
-      };
-
-      match print_evolutions(&client, args).await {
-        Ok(res) => assert_eq!(res, vals),
-        Err(err) => panic!("{}", err.render()),
-      }
-    }
-  }
-
-  #[tokio::test]
   async fn test_evolutions_allowed_natures() {
     let client = RustemonClient::default();
 
